50a056c8855cc77287d02457cc86f3547e398f47
cyril begard <cyril.begard@epitech.eu>
2020-10-14 18:31:44 +0200

    merge

--- app/src/main/java/com/example/epicture/MainActivity.java
+++ app/src/main/java/com/example/epicture/MainActivity.java
@@ -10,8 +10,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user);
-        Intent intent = new Intent(this, ConnectActivity.class);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, User.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/epicture/User.java
+++ app/src/main/java/com/example/epicture/User.java
@@ -41,7 +41,6 @@ public class User extends AppCompatActivity {
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
     }
 
