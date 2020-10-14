a8a2bd463c84876c2f79f07a0899f96ceff304d9
cyril begard <cyril.begard@epitech.eu>
2020-10-14 18:42:57 +0200

    merge

--- app/src/main/java/com/example/epicture/MainActivity.java
+++ app/src/main/java/com/example/epicture/MainActivity.java
@@ -11,7 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, User.class);
+        Intent intent2 = new Intent(this, User.class);
+        startActivity(intent2);
+        Intent intent = new Intent(this, ConnectActivity.class);
         startActivity(intent);
     }
 }
