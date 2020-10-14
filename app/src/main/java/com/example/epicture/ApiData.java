b85846f081c458751118cf213803866ca857a451
cyril begard <cyril.begard@epitech.eu>
2020-10-14 13:26:09 +0200

    merge

--- app/src/main/java/com/example/epicture/ApiData.java
+++
@@ -1,9 +0,0 @@
-package com.example.epicture;
-
-import net.azzerial.jmgur.api.Jmgur;
-
-public class ApiData {
-    public static final String CLIENT_ID = "95106cda05f6acb";
-    public static final String TAG = "Epicture";
-    public static Jmgur api = null;
-}
