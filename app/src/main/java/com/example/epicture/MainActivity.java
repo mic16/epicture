ce49da956ac3db86008fe3a5280292e69330d6a7
michael.fabrie <michael.fabrie@epitech.eu>
2020-10-19 16:10:53 +0200

    gallery slider

--- app/src/main/java/com/example/epicture/MainActivity.java
+++ app/src/main/java/com/example/epicture/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Intent intent2 = new Intent(this, User.class);
         startActivity(intent2);
-        Intent intent = new Intent(this, ConnectActivity.class);
-        startActivity(intent);
+        //Intent intent = new Intent(this, ConnectActivity.class);
+        //startActivity(intent);
     }
 }
--- app/src/main/java/com/example/epicture/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/epicture/ui/home/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,19 +19,33 @@ import com.example.epicture.R;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    public String test = "test";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        final TextView textView = root.findViewById(R.id.Favourites);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
         });
+
+        LinearLayout gallery = root.findViewById(R.id.gallery);
+
+        for (int i = 0; i < 6; i++) {
+
+            View galleryView = inflater.inflate(R.layout.gallery_item, gallery, false);
+
+            ImageView imageView = galleryView.findViewById(R.id.imageView);
+            //imageView.setImageResource(R.drawable.ic_launcher_background);
+
+            gallery.addView(galleryView);
+        }
+
         return root;
     }
 }
